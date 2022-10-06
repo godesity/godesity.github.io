403f2ddd85dc6bba1c3e85947c30ca868d26bd4c
Johannes Lindén <johannes@godesity.se>
2022-10-06 17:13:12 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Logo from './components/Logo';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Logo/index.js
@@ -0,0 +1,13 @@
+
+import logo from '../../logo.svg';
+import './Logo.module.css';
+
+
+const Logo = () => {
+    return (
+
+        <img alt="logo" src={logo}></img>
+    );
+}
+
+export default Logo;
