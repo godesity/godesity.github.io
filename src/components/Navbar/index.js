f9bada2dcc02df24ed7119787c2c01ac3ad87ec6
Johannes Lindén, GoDesity AB <johannes.linden@edisen.com>
2022-12-10 23:26:15 +0100

    Add logo animation in SVG format

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,5 @@
 import styles from "./Navbar.module.css";
 import { Outlet, Link, useLocation } from "react-router-dom";
-import gd from "../../assets/imgs/gd.png";
 
 console.log(styles)
 export default function Navbar() {
@@ -14,11 +13,25 @@ export default function Navbar() {
     <>
       <nav className={styles.nav}>
         <Link className="godesity-title" to="/">
-          <img height="60" src={gd} alt="GoDesity" valign="middle" />
+          <svg viewBox="139.852 76.482 86.452 60.502" xmlns="http://www.w3.org/2000/svg">
+            <polyline style={{ fill: "none", stroke: "#000", strokeWidth: "7px" }}
+              points="175 85 157 85 145 100 157 115 175 115">
+            </polyline>
+            <polyline style={{ fill: "none", stroke: "#000", strokeWidth: "7px" }}
+              points="165 105 181 105 181 125 195 125 207 109 195 93 175 93">
+            </polyline>
+            <polyline className={styles.path} style={{ fill: "none", stroke: "chocolate", opacity: '0.7', animationDelay: '1s', strokeWidth: "2px" }}
+              points="175 85 157 85 145 100 157 115 175 115">
+            </polyline>
+            <polyline className={styles.path} style={{ fill: "none", stroke: "chocolate", opacity: '0.7', animationDelay: '1s', strokeWidth: "2px" }}
+              points="165 105 181 105 181 125 195 125 207 109 195 93 175 93">
+            </polyline>
+          </svg>
+
         </Link>
         <input type="checkbox" id={styles.menucheck} />
         <ul className={styles.menu}>
-          <li className={styles.separator}><label for={styles.menucheck}>=</label></li>
+          <li className={styles.separator}><label htmlFor={styles.menucheck}>=</label></li>
           <li className={classNames('/omoss')}>
             <Link to="/omoss">Om oss</Link>
           </li>
