7cd768b102af1c604a09a1376e91e77b2744db0a
Johannes Lindén, GoDesity AB <johannes.linden@edisen.com>
2022-12-10 17:11:57 +0100

    Update menu style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,32 @@
 import "../assets/styles/navbar.css";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import gd from "../assets/imgs/gd.png";
 
 export default function Navbar() {
+  const { pathname } = useLocation();
+  const classNames = (path) => {
+    const paths = ['/omoss', '/projekt', '/kunder', '/kontakt'];
+    const idx = paths.indexOf(path);
+    return [pathname === path ? 'active' : '', paths.slice(idx).includes(pathname) ? 'line' : ''].filter(e => !!e).join(' ')
+  }
   return (
     <>
       <nav className="nav">
         <Link className="godesity-title" to="/">
-          <img height="60" src={gd} alt="GoDesity" valign="middle"/>
+          <img height="60" src={gd} alt="GoDesity" valign="middle" />
         </Link>
         <ul>
-          <li>
+          <li className="separator"></li>
+          <li className={classNames('/omoss')}>
             <Link to="/omoss">Om oss</Link>
           </li>
-          <li>
+          <li className={classNames('/projekt')}>
             <Link to="/projekt">Projekt</Link>
           </li>
-          <li>
+          <li className={classNames('/kunder')}>
             <Link to="/kunder">Kunder</Link>
           </li>
-          <li>
+          <li className={classNames('/kontakt')}>
             <Link to="/kontakt">Kontakt</Link>
           </li>
         </ul>
