3abaee7cb57022e7cc040782325a6660aab8c992
Johannes Lindén, GoDesity AB <johannes.linden@edisen.com>
2023-01-08 03:07:57 +0100

    Add faked effect of logo loading animation on all pages

--- src/layouts/Page/index.js
+++ src/layouts/Page/index.js
@@ -1,9 +1,11 @@
 import styles from "./Page.module.css";
+import Logo from '../../components/Logo';
 
 export default function Kunder(props) {
   const { title, children } = props;
   return (
     <div>
+      <div className={styles.loading}><Logo duration={10000} duration2={2000} /><div>Loading...</div></div>
       <div className={styles.titleStyle}>{title}</div>
       <div className={styles.contentBody}>
         {children}
