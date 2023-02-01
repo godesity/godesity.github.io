f0c77fd505451892209406193bc955a195028f9d
Johannes Lindén <johannes@godesity.se>
2023-02-01 23:26:43 +0100

    Update Kunder.js

--- src/routes/Kunder.js
+++ src/routes/Kunder.js
@@ -15,8 +15,7 @@ export default function Kunder() {
   }, [dispatch]);
   return (
     <Page title="Kunder">
-      Vi vill upprätthålla en god kommunikation till våra kunder. Därför finns det kontaktmöjligheter så som e-post och telefon. Du som kund kan när som helst få uppdateringar om hur utvecklingen av produkten går och vid varje milstolpe informerar vi dig om alternativa lösningar om nuvarande läge.
-      Om du vill komma i kontakt med oss så finns det här ett antal kontakt metoder.
+      Vi vill upprätthålla en god kommunikation till våra kunder. Därför finns det <Link to="/kontakt">kontaktmöjligheter</Link> så som e-post och telefon. Du som kund kan när som helst få uppdateringar om hur utvecklingen av produkten går och vid varje milstolpe informerar vi dig om alternativa lösningar om nuvarande läge.
       <CardList>
         {customerHighlights.map((c, i) =>
           <Card key={i} title={c.title.rendered} description={c.excerpt.rendered} coverImage={c.jetpack_featured_media_url} actionLink={"/kunder/" + c.id}></Card>
